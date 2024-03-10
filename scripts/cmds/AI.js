c7caba5961e94a56cfc56c7dd81c2cdb5b391854
Danielirik <162011788+Danielirik@users.noreply.github.com>
2024-03-10 14:53:16 +0100

    Ai.js

--- scripts/cmds/AI.js
+++ scripts/cmds/AI.js
@@ -1,44 +1,74 @@
-const axios = require("axios");
+const axios = require('axios');
 
-module.exports.config = {
-  name: "ai",
-  version: "1",
-  hasPermssion: 0,
-  credits: "Grey",
-  description: "ai",
-  commandCategory: "ai",
-  usages: "ai <ask>",
-  cooldowns: 5,
-};
-
-module.exports.run = async function({ api, event, args }) {
-  let { threadID, messageID, type, messageReply } = event;
-
-  if (type === "message_reply" && messageReply.attachments[0]?.type === "photo") {
-    const attachment = messageReply.attachments[0];
-    const imageURL = attachment.url;
+module.exports = {
+  config: {
+    name: "ai",
+    version: 2.0,
+    author: "OtinXSandip",
+    longDescription: "chat with ai",
+    category: "ai",
+    guide: {
+      en: "{p}{n} <Query>",
+    },
+  },
+  onStart: async function ({ message, event, Reply, args, api, usersData }) {
     try {
-      const res = await axios.get(`https://api.heckerman06.repl.co/api/other/img2text?input=${encodeURIComponent(imageURL)}`);
-      const response = res.data.extractedText;
-      const resAI = await axios.get(`https://api.heckerman06.repl.co/api/other/openai-chat?newprompt=${response}`);
-      const respondAI = resAI.data.content;
-      api.sendMessage(respondAI, threadID, messageID);
+      const id = event.senderID;
+      const userData = await usersData.get(id);
+      const name = userData.name;
+
+      const ment = [{ id: id, tag: name }];
+      const prompt = args.join(" ");
+      const encodedPrompt = encodeURIComponent(prompt);
+      api.setMessageReaction("⏰", event.messageID, () => {}, true);
+      const response = await axios.get(`https://sandyapi.otinxsandeep.repl.co/api/ai?query= ${encodedPrompt}`);
+      if (response.data && response.data.answer) {
+        const lado = response.data.answer;
+        api.setMessageReaction("✅", event.messageID, () => {}, true);
+        message.reply({
+          body: `${name}${lado}
+you can reply for continue chatting 🩷`,
+          mentions: ment,
+        }, (err, info) => {
+          global.GoatBot.onReply.set(info.messageID, {
+            commandName: this.config.name,
+            messageID: info.messageID,
+            author: event.senderID,
+          });
+        });
+      }
     } catch (error) {
-      api.sendMessage("Error occurred while fetching data from the API.", threadID, messageID);
+      console.error("Error:", error.message);
     }
-  } else {
-    const response = args.join(" ");
-    if (!response) {
-      api.sendMessage("Hi! What can I do for you?", threadID, messageID);
-      return;
-    }
-  
+  },
+  onReply: async function ({ message, event, Reply, args, api, usersData }) {
     try {
-      const res = await axios.get(`https://api.heckerman06.repl.co/api/other/openai-chat?newprompt=${response}`);
-      const respond = res.data.content;
-      api.sendMessage(respond, threadID, messageID);
+      const id = event.senderID;
+      const userData = await usersData.get(id);
+      const name = userData.name;
+
+      const ment = [{ id: id, tag: name }];
+      const prompt = args.join(" ");
+      const encodedPrompt = encodeURIComponent(prompt);
+     api.setMessageReaction("⏰", event.messageID, () => {}, true);
+      const response = await axios.get(`https://sandyapi.otinxsandeep.repl.co/api/ai?query=${encodedPrompt}`);
+      if (response.data && response.data.answer) {
+        const lado = response.data.answer;
+        api.setMessageReaction("✅", event.messageID, () => {}, true);
+        message.reply({
+          body: `${name} ${lado}
+you can reply for continue chatting 🩷`,
+          mentions: ment,
+        }, (err, info) => {
+          global.GoatBot.onReply.set(info.messageID, {
+            commandName: this.config.name,
+            messageID: info.messageID,
+            author: event.senderID,
+          });
+        });
+      }
     } catch (error) {
-      api.sendMessage("Error occurred while fetching data from the Chatgpt API.", threadID, messageID);
+      console.error("Error:", error.message);
     }
   }
 };
